e6971d3dbe78312d6355f2168f34c12f081b4056
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-28 20:58:09 -0300

    Using ChangeTracker - State: Unchanged and Modified

---
+++ 03_LearningEntityFramework/LearningEntityFramework/LojaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using static LearningEntityFramework.Program;
+
+namespace LearningEntityFramework
+{
+    //Criando contexto (Nome do modelo de negócio + sufixo Context) = LojaContext.
+    public class LojaContext : DbContext
+    {
+        //Informando quais classes serão persistidas, no nosso exemplo é Produto. 
+        //Informando o nome da tabela do banco de dados, no nosso exemplo é Produtos.
+        public DbSet<Produto> Produtos { get; set; }
+
+        //Definindo o banco de dados que será utilizado e o local. Para isso é precisso sobescrever
+        // o método da classe DbContext.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LojaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ 03_LearningEntityFramework/LearningEntityFramework/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+                foreach (var p in produtos)
+                {
+                    //listando todos os produtos
+                    Console.WriteLine(p);
+                }
+
+                Console.WriteLine("============");
+
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    //listando status do monitoramento
+                    Console.WriteLine(e);
+                }
+
+                //Alterando último produto da lista.
+                var p1 = produtos.Last();
+                p1.Nome = "Iphone 6";
+
+                Console.WriteLine("============");
+                // ChangeTracker o status de todas as entidades que sofreram alteração no contexto.
+                foreach (var e in contexto.ChangeTracker.Entries())
+                {
+                    Console.WriteLine(e);
+
+                    contexto.SaveChanges();
+                }
+
+            }
+                GetProdutosWithEntity();
+
+        }
+
+        private static void UpdadteProdutosWithEntity()
+        {
+            using (var repo = new ProdutoDAOWithEntity())
+            {
+                Produto firstProduct = repo.Produtos().First();
+                firstProduct.Nome = "Xiaomi Poco F2 Pró Editado";
+                repo.Update(firstProduct);
+
+
+            }
+        }
+
+        private static void DeleteProdutoWithEntity()
+        {
+            using (var repo = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+                Console.WriteLine($"Foram encontrados e removido {produtos.Count()} produtos(s)");
+                foreach (var item in produtos)
+                {
+                    repo.Delete(item);
+
+                }
+            }
+        }
+
+        private static void GetProdutosWithEntity()
+        {
+            using (var repo = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"Produto: {item.Nome}");
+                }
+            }
+        }
+
+        private static void SaveUsingEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Iphone 10";
+            p.Categoria = "Eletrônico";
+            p.Preco = 3000;
+
+            using (var contexto = new ProdutoDAOWithEntity())
+            {
+                contexto.Add(p);
+
+            };
+        }
+
+    }
+}
