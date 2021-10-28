cafcfbd4c1f44c908228a93a4b68da374079406c
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-28 11:03:59 -0300

    Saving data in database

---
+++ 00_LearningEntityFramework/LearningEntityFramework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LearningEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveUsingEntity();
+        }
+
+        private static void SaveUsingEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Xiami Poco F2 Pró";
+            p.Categoria = "Eletrônico";
+            p.Preco = 4000;
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+            };
+        }
+    }
+}
