5cc5b5694e420fc6488458866b72299506583a5d
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-29 10:58:09 -0300

    Using ChangeTracker - State: Added

---
+++ 05_LearningEntityFramework/LearningEntityFramework/ProdutoDAOWithEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEntityFramework
+{
+    public class ProdutoDAOWithEntity : IProdutoDAOWithEntity, IDisposable
+    {
+        private LojaContext Contexto;
+
+        public ProdutoDAOWithEntity()
+        {
+            this.Contexto = new LojaContext();
+        }
+
+        public void Add(Produto p)
+        {
+            Contexto.Produtos.Add(p);
+            Contexto.SaveChanges();
+        }
+
+        public void Delete(Produto p)
+        {
+            Contexto.Remove(p);
+            Contexto.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Contexto.Dispose();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            return Contexto.Produtos.ToList();
+        }
+
+        public void Update(Produto p)
+        {
+            Contexto.Update(p);
+            Contexto.SaveChanges();
+        }
+    }
+}
---
+++ 05_LearningEntityFramework/LearningEntityFramework/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var contexto = new LojaContext())
+            {
+
+                GetProdutosWithEntity();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                var newProduto = new Produto()
+                {
+                    Nome = "LG  k41s",
+                    Categoria = "Eletrônico",
+                    Preco = 800
+
+                };
+
+                contexto.Produtos.Add(newProduto);
+
+                //Mais uma forma de exibir o estado.
+                //foreach (var item in contexto.ChangeTracker.Entries())
+                //{
+                //    Console.WriteLine(item.State);
+                //}
+
+                contexto.SaveChanges();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                GetProdutosWithEntity();
+
+            }
+
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("==============");
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
+        }
+
+
+
+        private static void GetProdutosWithEntity()
+        {
+            using (var repo = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"Produto: {item.Nome}");
+                }
+            }
+        }
+    }
+}
+
