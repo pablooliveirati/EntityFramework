ececcdf019ebecf4d09593bf901e6dea208a1243
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-28 23:12:54 -0300

    logging sql on console

---
+++ 04_LearningEntityFramework/LearningEntityFramework/IProdutoDAOWithEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningEntityFramework
+{
+     interface IProdutoDAOWithEntity
+    {
+        void Add(Produto p);
+        void Update(Produto p);
+        void Delete(Produto p);
+        IList<Produto> Produtos();
+
+    }
+}
