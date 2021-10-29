f8ff4df6cae94856a35f22be7f2c9b38b2b390cd
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-29 11:18:24 -0300

    Using ChangeTracker - State: Deleted

---
+++ 07_LearningEntityFramework/LearningEntityFramework/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var contexto = new LojaContext())
+            {
+                var produtos = contexto.Produtos.ToList();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                var removep1 = produtos.First();
+                contexto.Produtos.Remove(removep1);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                //Mais uma forma de exibir o estado.
+                //foreach (var item in contexto.ChangeTracker.Entries())
+                //{
+                //    Console.WriteLine(item.State);
+                //}
+
+                contexto.SaveChanges();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+            }
+
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("==============");
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
+        }
+
+        private static void GetProdutosWithEntity()
+        {
+            using (var repo = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"Produto: {item.Nome}");
+                }
+            }
+        }
+    }
+}
+
