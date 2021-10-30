942860c5fd09eef21408dc411053204f325017ef
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-10-30 12:16:41 -0300

    Deleting an item from a DB list with Entity

---
+++ 08_LearningEntityFramework/LearningEntityFramework/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningEntityFramework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListarProdutos();
+            ExcluirProdutosPorId(2002);
+            Console.WriteLine("=============");
+            ListarProdutos();
+
+        }
+
+        private static void ListarProdutos()
+        {
+        using (var repo = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = repo.Produtos();
+
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine($"{item}");
+                }
+            }
+
+        }
+
+        private static void ExcluirProdutosPorId(int id)
+        {
+            using (var e = new ProdutoDAOWithEntity())
+            {
+                IList<Produto> produtos = e.Produtos();
+                var excluir = id;
+                foreach (var item in produtos)
+                {
+                    if (item.Id == id)
+                    {
+                        e.Delete(item);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
+
+
